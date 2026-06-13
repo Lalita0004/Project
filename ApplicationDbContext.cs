6e330e40fb3b8796fe744eecda146a2af122ab97
Lalita0004 <kumariaasu123456@gmail.com>
2026-06-13 12:36:44 +0530

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Clinic_Project.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinic_Project.DataAccess.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+    }
+
+    public DbSet<Department> Departments { get; set; }
+    public DbSet<Doctor> Doctors { get; set; }
+    public DbSet<Patient> Patients { get; set; }
+    public DbSet<DoctorSchedule> DoctorSchedules { get; set; }
+    public DbSet<TimeSlot> TimeSlots { get; set; }
+    public DbSet<Appointment> Appointments { get; set; }
+    public DbSet<Prescription> Prescriptions { get; set; }
+    public DbSet<Payment> Payments { get; set; }
+        public DbSet<Medicine> Medicines { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<LabTest> LabTests { get; set; }
+        public DbSet<LabBooking> LabBookings { get; set; }
+
+    }
+}
